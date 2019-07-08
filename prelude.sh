b44a72665069fb0e2fc7b318e28acea5e445f067
Adi Iyengar <aditya7iyengar@gmail.com>
2019-07-07 21:43:45 -0400

    Add prelude to override aditya7iyengar/adify

---
+++ prelude.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+#############
+### ABOUT ###
+#############
+# This script uses adify's prelude with Annkissam's setup scripts
+
+## Taken from aditya7iyengar/adify
+## Start----------------------
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
+_announce_step() {
+  echo -e """$BLUE
+==========================================================
+$1.......
+==========================================================$NC"""
+}
+
+_announce_error(){
+  echo -e """
+$RED[\u2717] $1 $NC
+  """
+  exit 1
+}
+
+_announce_success() {
+  echo -e """
+$GREEN[\u2713] $1 $NC
+  """
+}
+
+_announce_info() {
+  echo -e """$BLUE
+---> $1 $NC
+  """
+}
+## End----------------------
+
+fetch_ak_scripts() {
+  _announce_step "Checking if the system is Adifyable"
+
+  if [ -d "$HOME/.adify_scripts_for_annkissam" ]; then
+    _announce_error "${HOME}/.adify_scripts_for_annkissam found. Please delete the folder and restart"
+  else
+    _announce_info "Fetching Adify code to ${HOME}/.adify_scripts_for_annkissam"
+    git clone https://github.com/annkissam/adify_scripts_for_annkissam $HOME/.adify_scripts_for_annkissam
+  fi
+}
+
+run_adify_with_ak_configurations() {
+  _announce_step "Running adify with AK scripts"
+
+  TOOLS_DIR="${HOME}/.adify_scripts_for_annkissam/tools" bash <(wget -qO- https://raw.githubusercontent.com/aditya7iyengar/adify/master/prelude.sh)
+}
